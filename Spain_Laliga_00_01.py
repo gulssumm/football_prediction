30744b753309849a1086264d2a27505f70ee4470
gulssumm <gulsumcetinozlu@gmail.com>
2024-12-23 19:49:04 +0300

    spain laliga trials

--- Spain_Laliga_00_01.py
+++ Spain_Laliga_00_01.py
@@ -28,29 +28,29 @@ def get_element_text(selector_type, value):
 league_name = get_element_text("xpath", "//span/a")
 
 # Teams and score
-home_team = get_element_text("xpath", "//div[@class='sb-team sb-heim']//a")
-away_team = get_element_text("xpath", "//div[@class='sb-team sb-gast']//a")
+home_team = get_element_text("xpath", "//div[@class='sb-team sb-heim']//a[2]")
+away_team = get_element_text("xpath", "//div[@class='sb-team sb-gast']//a[2]")
 score = get_element_text("xpath", "//div[@class='sb-endstand']")
 
 # Match date
-match_date = get_element_text("xpath", "//div[contains(@class, 'spielbericht')]//span[@class='sb-datum']")
+match_date = get_element_text("xpath", "//div[contains(@class, 'sb-spieldaten')]//a[2]")
 
 # Extract players by position
 def get_players_by_position(position_class):
     try:
-        players = driver.find_elements(By.CLASS_NAME, position_class)
+        players = driver.find_elements(By.XPATH, position_class)
         return [player.text for player in players if player.text]
     except:
         return []
 
-goalkeepers = get_players_by_position("aufstellung-keeper")
+home_goalkeeper= get_players_by_position("//div[@class='large-12 columns']//table//tbody//tr//td[2]//a")
 defenders = get_players_by_position("aufstellung-verteidiger")
 midfielders = get_players_by_position("aufstellung-mittelfeld")
 forwards = get_players_by_position("aufstellung-angriff")
 
 # Manager
-home_manager = get_element_text("xpath", "//div[contains(@class, 'trainer-name-heim')]//a")
-away_manager = get_element_text("xpath", "//div[contains(@class, 'trainer-name-gast')]//a")
+home_manager = get_element_text("xpath", "/html/body/div[1]/main/div[5]/div/div/div[1]/div[2]/table/tbody/tr[5]/td[2]/a")
+away_manager = get_element_text("xpath", "/html/body/div[1]/main/div[5]/div/div/div[2]/div[2]/table/tbody/tr[5]/td[2]/a")
 
 # Step 4: Print the data
 print("League Name:", league_name)
@@ -58,7 +58,7 @@ print("Home Team:", home_team)
 print("Away Team:", away_team)
 print("Score:", score)
 print("Match Date:", match_date)
-print("Goalkeepers:", goalkeepers)
+print("Home Goalkeeper:", home_goalkeeper)
 print("Defenders:", defenders)
 print("Midfielders:", midfielders)
 print("Forwards:", forwards)
