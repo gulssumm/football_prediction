d556b8d26b22efe6a4b9803361620d5dd55a6164
gulssumm <gulsumcetinozlu@gmail.com>
2025-01-01 14:24:19 +0300

    csv files merged

--- basic_codes/merge_csvfiles.py
+++ basic_codes/merge_csvfiles.py
@@ -1,20 +1,38 @@
 import csv
+import chardet
 
 # List of CSV file names to merge
-file_names = ['2000_24_EN_ChampionshipLeague.csv',
-              '2000_24_EN_LeagueOne.csv', '2000_24_EN_PremierLeague.csv',
-              '2000_24_FR_Ligue1.csv', '2000_24_GER_Bundesliga.csv',
-              '2000_24_IT_serieA.csv', '2000_24_SP_laliga.csv',
-              '2000_24_TR_birincilig.csv', '2000_24_TR_superlig.csv']
+file_names = [
+    '../ENGLAND/2000_24_EN_ChampionshipLeague.csv',
+    '../ENGLAND/2000_24_EN_LeagueOne.csv',
+    '../ENGLAND/2000_24_EN_PremierLeague.csv',
+    '../FRANCE/2000_24_FR_Ligue1.csv',
+    '../GERMAN/2000_24_GER_Bundesliga.csv',
+    '../ITALY/2000_24_IT_serieA.csv',
+    '../SPAIN/2000_24_SP_laliga.csv',
+    '../TURKEY/2000_24_TR_birincilig.csv',
+    '../TURKEY/2000_24_TR_superlig.csv'
+]
 
 # Output file name
 output_file = 'merged_data.csv'
 
 # Merge CSV files
-with open(output_file, 'w', newline='') as outfile:
+with open(output_file, 'w', newline='', encoding='utf-8') as outfile:
     writer = csv.writer(outfile)
     for filename in file_names:
-        with open(filename, 'r') as infile:
-            reader = csv.reader(infile)
-            for row in reader:
-                writer.writerow(row)
+        try:
+            with open(filename, 'r', encoding='utf-8') as infile:  # Explicitly specify UTF-8 encoding
+                reader = csv.reader(infile)
+                for row in reader:
+                    writer.writerow(row)
+        except UnicodeDecodeError:
+            print(f"Error decoding file: {filename}. Check the file encoding.")
+
+
+
+"""file_name = '../ENGLAND/2000_24_EN_ChampionshipLeague.csv'
+with open(file_name, 'utf-8') as f:
+    raw_data = f.read()
+    result = chardet.detect(raw_data)
+    print(result['encoding'])"""
